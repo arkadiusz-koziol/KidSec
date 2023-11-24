b9c5616b1d66505cb0cdcdf9457a8d7e13dce75e
akoziol <a.koziol@grupavsc.pl>
2023-11-24 22:47:35 +0100

    init tips blade

---
+++ resources/views/tips.blade.php
@@ -0,0 +1,21 @@
+@extends('layouts.layout')
+
+@section('title', 'Porady')
+
+@section('content')
+    <div class="container mx-auto px-6 py-16">
+        <h2 class="text-4xl font-bold text-gray-800">Porady</h2>
+        <p class="mt-2 text-gray-600">Tutaj znajdziesz porady dotyczące bezpieczeństwa dzieci w sieci.</p>
+
+        <div class="mt-4">
+            <h3 class="text-xl font-semibold">Porada 1: Tytuł Porady</h3>
+            <p>Opis porady...</p>
+        </div>
+
+        <div class="mt-4">
+            <h3 class="text-xl font-semibold">Porada 2: Tytuł Porady</h3>
+            <p>Opis porady...</p>
+        </div>
+    </div>
+
+@endsection
