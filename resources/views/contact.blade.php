a4bd5bbb18268170c8ba5dfd915971beb88bdad2
akoziol <a.koziol@grupavsc.pl>
2023-11-25 18:19:24 +0100

    fix

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -3,14 +3,19 @@
 @section('title', 'Kontakt')
 
 @section('content')
-    <div class="container py-5">
-        <h2 class="contact-title">Kontakt</h2>
+    <div class="container my-5">
         <div class="row">
-            <div class="col-md-6 offset-md-3">
-                <div class="contact-info">
+            <div class="col-lg-8 mx-auto">
+                <div class="text-center mb-5">
+                    <h2 class="display-4">Skontaktuj się z Nami</h2>
+                    <p class="lead">Chętnie odpowiemy na Twoje pytania i wątpliwości.</p>
+                </div>
+
+                <div class="bg-light p-4 rounded shadow text-center">
+                    <h3 class="h5 text-primary mb-3">Dane Kontaktowe</h3>
+                    <p><strong>Nazwa uczelni:</strong> Akademia Humanistyczno-Ekonomiczna w Łodzi</p>
+                    <p><strong>Adres uczelni:</strong> Doktora Seweryna Sterlinga 26</p>
                     <p><strong>Email:</strong> <a href="mailto:contact@kidsec.pl">contact@kidsec.pl</a></p>
-                    <p><strong>Uczelnia:</strong> Akademia Humanistyczno-Ekonomiczna w Łodzi</p>
-                    <p><strong>Adres:</strong> Doktora Seweryna Sterlinga 26</p>
                     <p><strong>Telefon:</strong> 42 299 55 00</p>
                 </div>
             </div>
