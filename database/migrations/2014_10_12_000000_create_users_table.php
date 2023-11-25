89d7945a140f89779f893bb745687fba4c061435
akoziol <a.koziol@grupavsc.pl>
2023-11-25 13:50:35 +0100

    add timestamps to migration

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -33,6 +33,7 @@ return new class extends Migration
             $table->text('multi_factor_secret')
                 ->nullable();
             $table->softDeletes();
+            $table->timestamps();
         });
     }
 
