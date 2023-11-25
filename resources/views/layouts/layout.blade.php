7b35adc862734986d0c3a8824a1cb4eb336a12b0
akoziol <a.koziol@grupavsc.pl>
2023-11-25 16:31:34 +0100

    add logout button

--- resources/views/layouts/layout.blade.php
+++ resources/views/layouts/layout.blade.php
@@ -27,6 +27,10 @@
             <div class="flex flex-col md:flex-row md:mx-6">
                 @auth
                     <a href="{{ url('/topics/create') }}" class="my-1 text-sm text-gray-700 font-medium hover:text-indigo-500 md:mx-4 md:my-0">Dodaj Temat</a>
+                    <form method="POST" action="{{ route('logout') }}" class="my-1 md:mx-4 md:my-0">
+                        @csrf
+                        <button type="submit" class="text-sm text-gray-700 font-medium hover:text-indigo-500">Wyloguj się</button>
+                    </form>
                 @endauth
                 <a href="{{ url('/about') }}" class="my-1 text-sm text-gray-700 font-medium hover:text-indigo-500 md:mx-4 md:my-0">O Projekcie</a>
                 <a href="{{ url('/tips') }}" class="my-1 text-sm text-gray-700 font-medium hover:text-indigo-500 md:mx-4 md:my-0">Porady</a>
