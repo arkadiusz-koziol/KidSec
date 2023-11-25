8ca6197e3e3f63cde7b85c279d7430412865998b
akoziol <a.koziol@grupavsc.pl>
2023-11-25 18:24:57 +0100

    modify list blade

--- resources/views/topics/list.blade.php
+++ resources/views/topics/list.blade.php
@@ -9,11 +9,31 @@
 
         <div class="list-group mx-auto list-group-custom">
             @foreach($topics as $topic)
-                <a href="{{ url('/topics/' . $topic->id) }}" class="list-group-item list-group-item-action">
-                    <div class="list-item-title">{{ $topic->title }}</div>
-                    <div class="small text-muted mt-1">{{ $topic->subtitle }}</div>
-                </a>
+                <div class="list-group-item list-group-item-action d-flex justify-content-between align-items-center">
+                    <a href="{{ url('/topics/' . $topic->id) }}">
+                        <div class="list-item-title">{{ $topic->title }}</div>
+                        <div class="small text-muted mt-1">{{ $topic->subtitle }}</div>
+                    </a>
+
+                    @auth
+                        <a href="{{ url('/topics/' . $topic->id . '/edit') }}" class="btn btn-sm btn-outline-secondary">
+                            <i class="fas fa-pencil-alt"></i>
+                        </a>
+
+                        <button class="btn btn-sm btn-outline-danger ml-2" onclick="confirmDelete({{ $topic->id }})">
+                            <i class="fas fa-trash"></i>
+                        </button>
+                    @endauth
+                </div>
             @endforeach
         </div>
     </div>
+
+    <script>
+        function confirmDelete(id) {
+            if(confirm('Czy na pewno chcesz usunąć ten temat?')) {
+                window.location.href = '/topics/delete/' + id;
+            }
+        }
+    </script>
 @endsection
