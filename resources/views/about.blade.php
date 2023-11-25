421e03391511ae84eb4e9ea542dfacf8bc66f431
akoziol <a.koziol@grupavsc.pl>
2023-11-25 01:33:45 +0100

    fix content about

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -3,6 +3,17 @@
 @section('title', 'O Projekcie')
 
 @section('content')
-    <h2 class="text-4xl font-bold text-gray-800">O Projekcie</h2>
-    <p class="mt-2 text-gray-600">Informacje o projekcie KidSec...</p>
+    <div class="container py-5">
+        <div class="about-section">
+            <h2 class="text-center list-title">Projekt</h2>
+            <p class="text-muted text-center mb-5">Projekt powstał w ramach przedmiotu "Podstawy Filozofii - Projekt". Naszym celem jest zwiększenie świadomości zagrożeń w internecie wśród rodziców i dzieci.</p>
+
+            <h3>Zespół Projektowy</h3>
+            <p>Jesteśmy zespołem składającym się z sześciu dynamicznych i zaangażowanych osób: Arkadiusza Kozioła, Adriana Kowalczyka, Patrycji Królikowskiej, Michała Langnera, Tobiasza Dziuby oraz Bartłomieja Zabłockiego. Jesteśmy młodymi ludźmi, świadomymi wyzwań i niebezpieczeństw cyfrowego świata.</p>
+            <h3></h3>
+            <p>W naszym projekcie łączymy aktualną wiedzę o trendach internetowych z głębokim zrozumieniem zagrożeń, które mogą czyhać na młodych użytkowników sieci. Naszą misją jest przekazanie tej wiedzy rodzicom, aby mogli lepiej zrozumieć i chronić swoje dzieci przed cyfrowymi zagrożeniami.</p>
+            <h3></h3>
+            <p>Projekt "KidSec" koncentruje się na edukacji i uświadamianiu. Chcemy nauczyć dzieci, jak bezpiecznie poruszać się w internecie, jakie działania unikać i gdzie szukać pomocy w razie potrzeby, a zarazem rodziców tego, na co zwracać uwagę i jakie kroki warto podjąć, zanim stanie się tragedia. Wierzymy, że nasza inicjatywa przyczyni się do bezpieczniejszego środowiska online dla młodych osób.</p>
+        </div>
+    </div>
 @endsection
