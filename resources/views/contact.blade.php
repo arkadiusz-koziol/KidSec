7f133ba3406dad3f05bd5fd90a51846301e8baf7
akoziol <a.koziol@grupavsc.pl>
2023-11-25 17:50:23 +0100

    commit

---
+++ resources/views/contact.blade.php
@@ -0,0 +1,19 @@
+@extends('layouts.layout')
+
+@section('title', 'Kontakt')
+
+@section('content')
+    <div class="container py-5">
+        <h2 class="contact-title">Kontakt</h2>
+        <div class="row">
+            <div class="col-md-6 offset-md-3">
+                <div class="contact-info">
+                    <p><strong>Email:</strong> <a href="mailto:contact@kidsec.pl">contact@kidsec.pl</a></p>
+                    <p><strong>Uczelnia:</strong> Akademia Humanistyczno-Ekonomiczna w Łodzi</p>
+                    <p><strong>Adres:</strong> Doktora Seweryna Sterlinga 26</p>
+                    <p><strong>Telefon:</strong> 42 299 55 00</p>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
--- resources/views/topics/show.blade.php
+++ resources/views/topics/show.blade.php
@@ -6,7 +6,7 @@
     <div class="container py-5">
         <div class="card shadow-sm">
             <div class="card-body">
-                <h2 class="card-title">{{ $topic->title }}</h2>
+                <h1 class="card-title">{{ $topic->title }}</h1>
                 <h6 class="card-subtitle mb-2 text-muted">{{ $topic->subtitle }}</h6>
                 <div class="card-text mt-3">
                     {!! ($topic->content) !!}
